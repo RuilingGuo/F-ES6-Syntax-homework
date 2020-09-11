ae878d870c3c5a2c593bf1571acaa9fa7fb9a7a5
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-11 18:58:24 +0800

    [ruiling] feat: finish exercise

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = (input) => {
+    const {data} = input;
+    const output = [];
+    data.map((obj) => {
+        const [name, age, gender, birthday] = obj;
+        const result = { name, age, gender, birthday };
+        output.push(result)
+    })
+    return output;
+}
 export { parseData };
