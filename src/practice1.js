fe971c3cdf0be96ffc1d2e270844138c72708a4e
kevindongzg <dongkevin94@gmail.com>
2020-09-21 22:47:40 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,9 +1,13 @@
 const parseData = (input) => {
+    // TODO 解构可以优化到函数接受参数中
     const {data, column} = input;
+    // TODO keys 没有必要
     const keys = column.map(obj => obj.name)
+    // TODO 使用map做数组与数组的转化，不用声明output，查查map的使用
     const output = [];
     data.map((info) => {
         const values = {}
+        // TODO 这一层不是使用map的场景
         info.map((item,index) => {
             values[keys[index]] = item
         })
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,5 @@
 const inject = (items, sections) => {
+    // TODO 我们这里没有需求要保留items，可以不使用result，使用当然也没啥问题
     const result = [...items];
     sections
         .sort((s1, s2) => s2.index - s1.index)
